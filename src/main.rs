50e2c1c92aed3d9bca97a126382c2254a70efe78
Jerald Tomada <jeraldtomada@gmail.com>
2025-08-07 21:33:01 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::Write;
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -9,15 +9,36 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let result = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
-                match result {
-                    Ok(_result) => println!("success"),
-                    Err(e) => println!("error: {}", e)
-                }
+                let mut buffer = [0; 64];
+                let _bytes_read = stream.read(&mut buffer).unwrap();
+                let client_request: &str = std::str::from_utf8(&buffer).unwrap();
+                println!("read string: {}", client_request);
+                handle_client_request(&mut stream, client_request);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_client_request(stream: &mut TcpStream, request: &str) {
+    let lines: Vec<&str> = request.split("\r\n").collect();
+    let request_line = lines[0];
+    let words: Vec<&str> = request_line.split_whitespace().collect();
+    let request_target = words[1];
+    println!("request target: {} size: {}", request_target, request_target.len());
+    if request_target.len() > 1 {
+        let result = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n");
+        match result {
+            Ok(_result) => println!("success"),
+            Err(e) => println!("error: {}", e)
+        }
+    } else {
+        let result = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+        match result {
+            Ok(_result) => println!("success"),
+            Err(e) => println!("error: {}", e)
         }
     }
 }
