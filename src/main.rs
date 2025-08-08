ce8893c2a4f89a0637045b6a85bccda6bc10854b
Jerald Tomada <jeraldtomada@gmail.com>
2025-08-07 20:35:03 -0700

    send response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -6,8 +7,13 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let result = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                match result {
+                    Ok(_result) => println!("success"),
+                    Err(e) => println!("error: {}", e)
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
