4124f5c60a2de8d8fb48fc2d2be557d4756ed565
Jerald Tomada <jeraldtomada@gmail.com>
2025-08-07 21:38:18 -0700

    clippy fix

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,11 @@ fn main() {
                 let mut buffer = [0; 64];
                 let _bytes_read = stream.read(&mut buffer).unwrap();
                 let client_request: &str = std::str::from_utf8(&buffer).unwrap();
-                println!("read string: {}", client_request);
+                println!("read string: {client_request}");
                 handle_client_request(&mut stream, client_request);
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {e}");
             }
         }
     }
@@ -27,18 +27,18 @@ fn handle_client_request(stream: &mut TcpStream, request: &str) {
     let request_line = lines[0];
     let words: Vec<&str> = request_line.split_whitespace().collect();
     let request_target = words[1];
-    println!("request target: {} size: {}", request_target, request_target.len());
+    println!("request target: {request_target} size: {}", request_target.len());
     if request_target.len() > 1 {
         let result = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n");
         match result {
             Ok(_result) => println!("success"),
-            Err(e) => println!("error: {}", e)
+            Err(e) => println!("error: {e}")
         }
     } else {
         let result = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
         match result {
             Ok(_result) => println!("success"),
-            Err(e) => println!("error: {}", e)
+            Err(e) => println!("error: {e}")
         }
     }
 }
